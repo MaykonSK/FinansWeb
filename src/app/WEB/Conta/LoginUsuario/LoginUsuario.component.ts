bc2140a308d2ce51fe45ceabd62fa7de96f98f1c
Maycon Rodrigo <mayconrodrigo123@hotmail.com>
2022-10-12 22:31:38 -0300

    reCaptcha login

--- src/app/WEB/Conta/LoginUsuario/LoginUsuario.component.ts
+++ src/app/WEB/Conta/LoginUsuario/LoginUsuario.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, NgZone, OnInit, Output, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { TokenService } from 'src/app/Autenticacao/token.service';
+import { environment } from 'src/environments/environment';
 import { WebService } from '../../web.service';
+
+const KeyCaptchaSite = environment.CaptchaKeySite;
 
 @Component({
   selector: 'app-LoginUsuario',
@@ -16,7 +19,17 @@ export class LoginUsuarioComponent implements OnInit {
   public mensagemError: string = ""
   public mensagemSuccess: string = ""
 
-  constructor(private fb: FormBuilder, private service: WebService, private tokenService: TokenService, private router: Router) {}
+  @ViewChild('divRecaptcha')
+  divRecaptcha!: ElementRef<HTMLDivElement>;
+
+  get grecaptcha(): any {
+    const w = window as any;
+    return w['grecaptcha'];
+  }
+
+  constructor(private fb: FormBuilder, private service: WebService, private tokenService: TokenService, private router: Router, private ngZone: NgZone) {
+    this.renderizarReCaptcha();
+  }
 
   ngOnInit() {
     this.login;
@@ -24,13 +37,16 @@ export class LoginUsuarioComponent implements OnInit {
 
   login = this.fb.group({
     Email: [null, [Validators.required, Validators.email]],
-    Password: [null, Validators.required]
+    Password: [null, Validators.required],
+    recaptcha: [null, Validators.required]
   })
 
   LogarUsuario() {
     if (this.login.valid) {
       this.Loading = true;
       const dados = this.login.getRawValue();
+      console.log(dados);
+
       this.service.logarUsuario(dados).subscribe(token => {
         var tokenFormatado = JSON.parse(JSON.stringify(token)).message.split(' ')[0]
         this.tokenService.salvaToken(tokenFormatado)
@@ -41,6 +57,53 @@ export class LoginUsuarioComponent implements OnInit {
         this.mensagemError = error.error.message;
       })
     }
+  }
+
+  renderizarReCaptcha() {
+    // *
+    // * Para evitar que change detection seja disparado
+    // * cada vez que o setTimeout for executado,
+    // * executamos essa recorrência fora da zona
+    // * do Angular, por isso o usamos o runOutsideAngular
+    // *
+    // * Para saber mais sobre change detection:
+    // * https://consolelog.com.br/como-funciona-change-detection-angular/
+    // *
+    this.ngZone.runOutsideAngular(() => {
+      // *
+      // * Se o "grecaptcha" ainda não foi carregado ou
+      // * o elemento <div> onde o reCAPTCHA será
+      // * renderizado ainda não foi construído,
+      // * aguardamos algum tempo e executamos novamente
+      // * este método:
+      // *
+      if (!this.grecaptcha || !this.divRecaptcha) {
+        setTimeout(() => {
+          this.renderizarReCaptcha();
+        }, 500);
+
+        return;
+      }
+
+      // * Se chegou aqui é porque o recaptcha já está
+      // * carregado. Então solicitamos sua renderização
+      // * na tela.
+      const idElemento =
+        this.divRecaptcha.nativeElement.getAttribute('id');
+
+      this.grecaptcha.render(idElemento, {
+        sitekey: KeyCaptchaSite,
+        callback: (response: any) => {
+          // * Este método é chamado quando o usuário
+          // * resolver o desafio do CAPTCHA
+          console.log(response);
+
+          this.ngZone.run(() => {
+            this.login.get('recaptcha')?.setValue(response);
+          });
+        },
+      });
+    });
   }
 
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   UsuariosAPI: "https://localhost:6001",
   FinansAPI: "https://localhost:5001/api",
+  CaptchaKeySite: "6Lcmq3MiAAAAADTEHnBzAwSl1wko9aK9aUMX0L11",
   production: false
 };
 
